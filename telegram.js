829f56bb2d9d7157b53778bc2149be0a8285f31b
nergo <chelovek27okno@gmail.com>
2025-07-11 00:17:02 +0300

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -10,8 +10,9 @@ document.getElementById('closeBtn').addEventListener('click', () => {
   tg.close();
 });
 
-// Можно использовать данные пользователя
+// Использование данных пользователя
 const user = tg.initDataUnsafe.user;
 if (user) {
   console.log(`Привет, ${user.first_name}!`);
+  document.getElementById('username').textContent = user.first_name; // Пример использования
 }
